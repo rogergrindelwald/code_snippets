fbe31335755008cc708502c3d53f5fd4c0658543
rogergrindelwald <rogergrindelwald@gmail.com>
2016-03-21 21:14:01 -0400

    Update regex.py

--- python/regex.py
+++ python/regex.py
@@ -3,3 +3,8 @@ QUIT_NO_CASE = re.compile('quit', re.IGNORECASE)
 command = input('Type Command:')
 if QUIT_NO_CASE.match(command):
   print("Quiting...")
+
+# Dissect multiple pieces from a string, using re.split.
+PHONE_NUMBER_REG = re.compile('(\d+)-(\d+)-(\d+)')
+# 0: area code, 1: extension1, 2: extension2
+items = PHONE_NUMBER_REG.split(phone_number)
