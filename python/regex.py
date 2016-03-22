6103333acf628464ac377d93f58d81fc52d56d85
rogergrindelwald <rogergrindelwald@gmail.com>
2016-03-21 21:05:15 -0400

    Update regex.py

--- python/regex.py
+++ python/regex.py
@@ -1,2 +1,5 @@
 # re.IGNORECASE can be used for allowing user to type arbitrary cased texts.
 QUIT_NO_CASE = re.compile('quit', re.IGNORECASE)
+command = input('Type Command:')
+if QUIT_NO_CASE.match(command):
+  print("Quiting...")
